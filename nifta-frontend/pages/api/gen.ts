9e297e02f57a4c244ee5ba43600b157d3d701864
wisdom nwakaku <nwakakucc@gmail.com>
2023-06-12 12:05:16 +0100

    somelast

--- nifta-frontend/pages/api/gen.ts
+++ nifta-frontend/pages/api/gen.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
-import 'dotenv/config'
+
+require('dotenv').config()
 
 const configuration = new Configuration({
-  apiKey: "",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
